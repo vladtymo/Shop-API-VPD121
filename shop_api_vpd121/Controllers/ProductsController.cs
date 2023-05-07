d95a8c536cb06634a982181c687b63b90cb7a538
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2023-05-07 14:57:17 +0300

    Add project files.

---
+++ shop_api_vpd121/Controllers/ProductsController.cs
@@ -0,0 +1,77 @@
+﻿using Data;
+using Data.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace shop_api_vpd121.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ShopDbContext context;
+
+        public ProductsController(ShopDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]                       // GET: ~/api/products
+        //[HttpGet("all")]              // GET: ~/api/products/all
+        //[HttpGet("/all-products")]    // GET: ~/all-products
+        public IActionResult Get()
+        {
+            return Ok(context.Products.ToList());
+        }
+
+        // Details
+        [HttpGet("details/{id}")]
+        public IActionResult Get(int id)
+        {
+            if (id < 0) return BadRequest();
+
+            var product = context.Products.Find(id);
+
+            if (product == null) return NotFound();
+
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            context.Products.Add(product);
+            context.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult Edit(Product product)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            context.Products.Update(product);
+            context.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            if (id < 0) return BadRequest();
+
+            var product = context.Products.Find(id);
+
+            if (product == null) return NotFound();
+
+            context.Products.Remove(product);
+            context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
---
+++ shop_api_vpd121/Program.cs
@@ -0,0 +1,32 @@
+using Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+string connStr = builder.Configuration.GetConnectionString("LocalDb");
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ShopDbContext>(x => x.UseSqlServer(connStr));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
